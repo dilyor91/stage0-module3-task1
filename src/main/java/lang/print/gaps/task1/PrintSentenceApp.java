9f42fd33e2999374cf78951dd6b6939d1f00ab44
dilyor91 <dilyor_05@mail.ru>
2022-08-01 18:48:19 +0500

    Task #1. Syntax v2

--- src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
+++ src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintSentenceApp {
     public static void main(String[] args) {
-        System.out.println("This\n" +  "is\n"  + "my\n" +"first\n"+ "Java\n" +"program\n");
+        System.out.print("This is my first Java program\n");
     }
 }
